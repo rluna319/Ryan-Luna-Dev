74d29d7654366ce23f8f12e8b3bbe7fae8acfcb0
rluna319 <ryanluna319@gmail.com>
2024-09-19 18:42:11 -0500

    fix: syntax and other in `cleanup-deployments.mjs`
    - now using `.mjs` extension for cleanup-deployments JS so npm can recognize it as ES module
    	- [@octokit/core](https://www.npmjs.com/package/octokit) npm package is an ESM and not compatible with CommonJS
    - removed `scripts/cleanup-deployments.mjs` from deployment scripts to restrict it to manual usage
    	- It might be better to track deployments using semantic versioning and look neater anyway.

--- scripts/cleanup-deployments.mjs
+++ scripts/cleanup-deployments.mjs
@@ -1,4 +1,5 @@
-const { Octokit } = require("@octokit/core");
+// const { Octokit } = require("@octokit/core"); // CommonJS
+import { Octokit } from "@octokit/core";
 const owner = "rluna319";
 const repo = "Developer-Portfolio";
 const token = process.env.DEV_PORTFOLIO_GITHUB_API_TOKEN;
@@ -17,24 +18,28 @@ async function deleteInactiveDeployments() {
 
     for (const deployment of deployments.data) {
       const deployment_id = deployment.id;
+
+      console.log('Checking deployment:', deployment_id);
       
       // Check the status of each deployment
       const statuses = await octokit.request('GET /repos/{owner}/{repo}/deployments/{deployment_id}/statuses', {
         owner,
         repo,
-        deployment_id,
+        deployment_id
       });
 
-      const isActive = statuses.data.some(status => status.state === 'success');
+      const isInactive = statuses.data[0].state === 'inactive';
       
       // If not active, delete the deployment
-      if (!isActive) {
+      if (isInactive) {
         await octokit.request('DELETE /repos/{owner}/{repo}/deployments/{deployment_id}', {
           owner,
           repo,
           deployment_id,
         });
         console.log(`Deleted inactive deployment: ${deployment_id}`);
+      } else {
+        console.log(`Deployment is still active, skipping...`);
       }
     }
   } catch (error) {
